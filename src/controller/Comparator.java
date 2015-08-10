021b72d5922dd67cd076320a9cb18b9b83363e19
Isadora Pedrini Possebon <Isadora@Isadoras-MacBook-Pro.local>
2015-08-10 15:50:08 +0200

    Two steps algorithm implemented

--- src/controller/Comparator.java
+++ src/controller/Comparator.java
@@ -5,13 +5,19 @@ import java.util.ArrayList;
 import model.Graph;
 
 public class Comparator {
+	private static final int comparison_factor = 1;
 
 	public Comparator(){
 		
 	}
 	
-	/* Returns the degree of similiarity between two graphs. */
-	public int compare(Graph g1, Graph g2){
+	/* For each node in g1: 
+	 * 		- create 'connections' that contains g1 connections;
+	 * 		- get correspondent node in g2, if one exists;
+	 * 			- store its connections in 'connections2'.
+	 * 		- compare 'connections' and 'connections2': for each similar connection: +1.
+	 * Returns the degree of similiarity between two graphs. */
+	public int myCompare(Graph g1, Graph g2){
 	int degree = 0;
 	String g1_nodes[][] = g1.getNodes();
 		
@@ -44,8 +50,38 @@ public class Comparator {
 				degree = evaluate(connections_names, connections_names2);
 			}
 		}
-		System.out.println("Similarity degree: " + degree);
 		return degree;
+	}
+	
+	/* First step: compare nodes.
+	 * Second step: compare only graphs that have at least n similar nodes. (n is indicated by 'comparison_factor') */
+	public int twoStepsCompare(Graph g1, Graph g2){
+		ArrayList<String> similar_nodes = new ArrayList<String>();
+		similar_nodes = getSimilarNodes(g1, g2);
+		
+		if (similar_nodes.size() < comparison_factor){
+			return 0;
+		}
+		return myCompare(g1, g2);
+	}
+	
+	private ArrayList<String> getSimilarNodes(Graph g1, Graph g2){
+		ArrayList<String> similar_nodes = new ArrayList<String>();
+		String g1_nodes[][] = g1.getNodes();
+		String g2_nodes[][] = g2.getNodes();
+		
+		for (int i = 0; i < g1.getNumb_nodes(); i++){
+			String name1 = g1_nodes[i][0];
+			
+			for (int j = 0; j <g2.getNumb_nodes(); j++){
+				String name2 = g2_nodes[j][0];
+				
+				if (name1.equals(name2)){
+					similar_nodes.add(name1);
+				}
+			}
+		}
+		return similar_nodes;
 	}
 	
 	private int evaluate(ArrayList<String> nodes1, ArrayList<String> nodes2){
--- src/controller/Main.java
+++ src/controller/Main.java
@@ -13,26 +13,31 @@ public class Main {
 		System.out.println("Number of graphs: " + parser.getGraphs().size());
 		
 		for (Graph g : parser.getGraphs()){
-			//System.out.println("-------------");
-			//parser.getGraphs().get(i).printInfo();
 			g.createAdjacencyMatrix();
-			//parser.getGraphs().get(i).printAdjancencyMatrix();
-			//System.out.println("-------------");
 		}
-		
 		
 		// Comparision
 		Comparator comparator = new Comparator();
+		/*
 		for (int i = 0; i < (int)parser.getGraphs().size()/2; i++){
 			System.out.println("Comparision between " + parser.getPeople().get(i).getName() + " and " + parser.getPeople().get(i+1).getName());
-			comparator.compare(parser.getPeople().get(i).getUndirected_graph(), parser.getPeople().get(i+1).getUndirected_graph());
+			comparator.myCompare(parser.getPeople().get(i).getUndirected_graph(), parser.getPeople().get(i+1).getUndirected_graph());
 			System.out.println("-----------------");
 		}
-		
+		*/
 		// TEST data/xam
+		/*
 		System.out.println("Comparision between DIRECTED AND UNDIRECTED GRAPHS: " + parser.getPeople().get(12).getName());
-		comparator.compare(parser.getPeople().get(12).getDirected_graph(), parser.getPeople().get(12).getUndirected_graph());
+		comparator.myCompare(parser.getPeople().get(12).getDirected_graph(), parser.getPeople().get(12).getUndirected_graph());
 		System.out.println("-----------------");
+		*/
 		
+		for (int i = 0; i < (int) parser.getGraphs().size() - 1; i++){
+			int degree = comparator.twoStepsCompare(parser.getGraphs().get(i),  parser.getGraphs().get(i+1));
+			
+			if (degree > 2){
+				System.out.println("Comparison between " + i + " and " + i+1 + " --> Similarity degree: " + degree);
+			}
+		}
 	}
 }
